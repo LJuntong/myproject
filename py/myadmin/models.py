051b92a63aee0395ca8f6da1afd8f4370577375c
yuanyuan <13132198676@163.com>
2018-06-25 23:50:09 +0800

    商场项目，已完成步骤至商品添加

---
+++ py/myadmin/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+
+#会员模型
+class Users(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=80)
+    email = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    age = models.IntegerField(null=True)
+    sex = models.CharField(max_length=1,null=True)
+    pic = models.CharField(max_length=100,null=True)
+    #０正常　1禁用
+    status = models.IntegerField(default=0)
+    addtime = models.DateTimeField(auto_now_add=True)
+
+
+#商品分类模型
+class Types(models.Model):
+    '''
+       无限分类
+       types
+          id  name  pid path
+          1   服装　  0   0,
+          2   男装　　　1   0,1,
+          3   西服　　　2   0,1,2,
+          4   西裤　　　3   0,1,2,3,
+    '''
+    name = models.CharField(max_length=20)
+    pid = models.IntegerField()
+    path = models.CharField(max_length=50)
+
+
+# 商品模型
+class  Goods(models.Model):
+    # 一对多
+    typeid =  models.ForeignKey(to="Types", to_field="id")
+    # 标题
+    title = models.CharField(max_length=255)
+
+    descr = models.CharField(max_length=255,null=True)
+    # 信息
+    info = models.TextField(null=True)
+    #价格
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    pics = models.CharField(max_length=100)
+    ##状态 0 新发布,1下架
+    status = models.IntegerField(default=0)
+    #存储
+    store = models.IntegerField(default=0)
+    #订单数量
+    num = models.IntegerField(default=0)
+    #用户点击量
+    clicknum = models.IntegerField(default=0)
+    #上传时间
+    addtime = models.DateTimeField(auto_now_add=True)
+
