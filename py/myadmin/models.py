7feb85cd9d230460b35df6be875ec41e954c0946
yuanyuan <13132198676@163.com>
2018-06-30 23:39:43 +0800

    商场项目　购物车功能不够完善

--- py/myadmin/models.py
+++ py/myadmin/models.py
@@ -16,6 +16,15 @@ class Users(models.Model):
     addtime = models.DateTimeField(auto_now_add=True)
 
 
+# 会员地址
+class Address(models.Model):
+    uid =  models.ForeignKey(to="Users", to_field="id")
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    address = models.CharField(max_length=20)
+    xiangxi = models.CharField(max_length=50)
+    status = models.IntegerField(default=0)
+
 #商品分类模型
 class Types(models.Model):
     '''
@@ -58,4 +67,34 @@ class  Goods(models.Model):
     clicknum = models.IntegerField(default=0)
     #上传时间
     addtime = models.DateTimeField(auto_now_add=True)
+
+
+
+# 订单模型
+class Orders(models.Model):
+    uid = models.ForeignKey(to="Users", to_field="id")
+    addressid = models.ForeignKey(to="Address", to_field="id")
+    totalprice = models.FloatField()
+    totalnum = models.IntegerField()
+    status = models.IntegerField(default=0)
+    addtime = models.DateTimeField(auto_now_add=True,null=True)
+
+# 订单详情
+class OrderInfo(models.Model):
+    orderid = models.ForeignKey(to="Orders", to_field="id")
+    gid = models.ForeignKey(to="Goods", to_field="id")
+    num = models.IntegerField()
+        
+
+'''订单模型
+    id  uid  addressid  totalprice totalnum status 
+    1000   12    2          2000        5        0
+'''
+
+''' 订单详情
+    id  orderid    gid   num 
+    1    1000       9      2
+    2    1000       10     3
+'''
+
 
--- py/myhome/views.py
+++ py/myhome/views.py
@@ -5,7 +5,9 @@ from myadmin.models import Users,Types,Goods
 
 from django.contrib.auth.hashers import make_password, check_password
 
-#首页
+# Create your views here.
+
+# 首页
 def index(request):
     '''
         [
@@ -39,21 +41,22 @@ def index(request):
             },
         ]
     '''
-    #先获取所有的顶级分类
+
+    # 先获取所有的顶级分类
     data = Types.objects.filter(pid=0)
 
     erdata = []
     for x in data:
-        ##循环遍历出来所有的顶级分类
-        # print(x.name)
-
-        #获取当前类下的子类 二级分类
+        # 获取当前类下的子类
         x.sub = Types.objects.filter(pid=x.id)
-        # print(x.sub)
         for v in x.sub:
             #获取当前子类下的商品 
             v.goodssub = Goods.objects.filter(typeid=v.id)
             erdata.append(v)
+
+
+
+
     context = {'typegoodslist':data,'erdata':erdata}
     return render(request,'myhome/index.html',context)
 
@@ -69,6 +72,7 @@ def list(request,tid):
 
     return render(request,'myhome/list.html',context)
 
+
 # 搜索
 def search(request):
 
@@ -83,25 +87,28 @@ def search(request):
 
     context = {'goodslist':data}
 
-    return render(request,'myhome/search.html',context) 
+    return render(request,'myhome/search.html',context)
+
 
 # 详情
 def info(request,sid):
-    
     try:
         # 根据商品id获取商品信息
         data = Goods.objects.get(id=sid)
+
+        # 修改当前商品的点击量
+        data.clicknum = data.clicknum+1
+        data.save()
 
         context = {'ginfo':data}
         return render(request,'myhome/info.html',context)
         
     except:
         pass
-
-
-#登录
-def login(request):
     
+
+# 登录
+def login(request):
     if request.method == "GET":
         return render(request,'myhome/login.html')
 
@@ -123,18 +130,9 @@ def login(request):
         
         return HttpResponse('<script>alert("用户名或密码错误");history.back(-1)</script>')
 
-#退出登录
-def logout(request):
-    
-    request.session['VipUser'] = {}
-    
-    return HttpResponse('<script>alert("退出成功");location.href="/"</script>')
 
 
-
-
-
-#注册页面
+# 注册
 def register(request):
     if request.method == 'GET':
         return render(request,'myhome/register.html')
@@ -171,10 +169,16 @@ def register(request):
 
 
 
+# 退出
+def logout(request):
+    request.session['VipUser'] = {}
+    
+    return HttpResponse('<script>alert("退出成功");location.href="/"</script>')
 
-
-#显示验证码
+        
+# 验证码
 def vcode(request):
+    
     #引入绘图模块
     from PIL import Image, ImageDraw, ImageFont
     #引入随机函数模块
@@ -219,3 +223,128 @@ def vcode(request):
     im.save(buf, 'png')
     #将内存中的图片数据返回给客户端，MIME类型为图片png
     return HttpResponse(buf.getvalue(), 'image/png')
+
+
+
+
+# 加入购物车
+def addcart(request):
+    # 获取商品id,商品购买数量
+    sid = request.GET['sid']
+    num = int(request.GET['num'])
+
+    # 先获取购物车的商品数据
+    data = request.session.get('cart',{})
+
+    # 判断要购物的商品是否已经存在购物车里
+    if sid in data.keys():
+        # 存在,修改数量
+        data[sid]['num'] += num
+    else:
+        # 不存在,添加
+        # 定义加入购物车的商品数据格式
+        ob = Goods.objects.get(id=sid)
+        goods = {'id':ob.id,'title':ob.title,'price':float(ob.price),'pics':ob.pics,'num':num}
+        # 把商品加入购物车
+        data[sid] = goods
+
+    # 把购物车存入session
+    request.session['cart'] = data
+
+    return HttpResponse('1')
+
+
+# 购物车列表
+def cartlist(request):
+    data = request.session['cart'].values()
+
+
+    return render(request,'myhome/cart.html',{'data':data})
+
+
+# 清空购物车
+def cartclear(request):
+    request.session['cart'] = {}
+
+    return HttpResponse('<script>location.href="/cartlist/"</script>')
+
+# 删除购物车的商品
+def delcart(request):
+    sid = request.GET['sid']
+    # 获取sessin中的购物车数据
+    data = request.session['cart']
+
+    del data[sid]
+
+    # 把购物车重新写入session
+    request.session['cart'] = data 
+
+    return HttpResponse('0')
+
+
+
+# 修改购物车数量
+def editcart(request):
+    sid = request.GET['sid']
+    num = int(request.GET['num'])
+
+    #在session获取购物车数据
+    data = request.session['cart']
+
+    # 修改
+    data[sid]['num'] = num
+
+    # 把购物车重新写入session
+    request.session['cart'] = data
+
+    return HttpResponse('0')
+
+
+
+
+
+
+# # session测试
+# def sesset(request):
+    
+#     # 设置session
+#     # request.session['abc'] = 'aabbcc'
+#     # request.session['def'] = 'ddeeff'
+#     # request.session['VipUser'] = {'uid':1}
+
+#     # request.session['name'] ='小明'
+
+
+#     request.session['age'] = 20   
+#     request.session.set_expiry(10)
+
+
+#     return HttpResponse('设置session')
+
+# def sesget(request):
+    
+#     # 获取session数据
+#     # 获取session中键=abc的数据,如果不存在,报错
+#     # print(request.session['abc'])
+#     # print(request.session.get('abc',None))
+
+#     print(request.session.values())
+#     # 删除session
+
+#     # 删除单个key  如果key不存在,则报错
+#     # del request.session['VipUser']
+
+#     # if not request.session.get('VipUser',None):
+#     #     print('退出登录')
+
+
+#     # #清除所有会话,但不会删除数据库中会话数据
+#     # request.session.clear() 
+
+
+#     #删除当前的会话数据
+#     # request.session.flush()
+
+
+#     return HttpResponse('获取session')
+
